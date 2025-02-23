d2c584394b9bb02ef8b7b37fd76d203139f85fa7
hakehardware <ops@hakedev.com>
2025-02-22 17:04:54 -0700

    added comment

--- example.py
+++ example.py
@@ -4,11 +4,12 @@ import time
 
 if __name__ == "__main__":
     x = X()
-    logger.info("Posting to X")
     success = False
     while not success:
         response = x.create_post("Hello World!")
         if response == "success": continue
         elif response == "Not Authorized":
             print("App not authorized, will retry in 60 seconds")
+
+        # Sleep 60 seconds and try again
         time.sleep(60)
